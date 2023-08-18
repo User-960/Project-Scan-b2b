f2dca60c3e0224a4936e80715ba1e6f893b65d9b
Ivan <ivanvolodin960@gmail.com>
2023-08-19 00:27:08 +0300

    feat: add variables

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,9 +1,20 @@
+import localFont from 'next/font/local'
 import { FC } from 'react'
 
 import styles from './Home.module.scss'
 
+const ferryFont = localFont({
+	src: '../../../assets/fonts/ferry_black.otf',
+	display: 'swap'
+})
+
 const Home: FC = () => {
-	return <div className={styles.title}>Home</div>
+	return (
+		<>
+			<div className={styles.title}>Home</div>
+			<div className={ferryFont.className}>Page</div>
+		</>
+	)
 }
 
 export default Home
