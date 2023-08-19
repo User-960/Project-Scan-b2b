a5708b40037cbaa6762877643fafc92f53a62045
Ivan <ivanvolodin960@gmail.com>
2023-08-19 13:43:01 +0300

    feat: add component info for account

--- src/components/layout/header/account/Account.tsx
+++ src/components/layout/header/account/Account.tsx
@@ -16,26 +16,45 @@ const Account: FC<IAccount> = ({ isAuthorized = false }) => {
 		<>
 			{isAuthorized ? (
 				<div className={styles.enter}>
-					<button className={styles.registration} onClick={() => push('#')}>
+					<button
+						className={styles.registration}
+						onClick={() => push('/registration')}
+					>
 						Зарегистрироваться
 					</button>
 					|
-					<button className={styles.btnLogin} onClick={() => push('#')}>
+					<button className={styles.btnLogin} onClick={() => push('/login')}>
 						Войти
 					</button>
 				</div>
 			) : (
 				<div className={styles.account}>
-					<div className={styles.profile}>
-						<div className={styles.name}>Алексей А. </div>
-						<Image
-							src={avatar}
-							width={32}
-							height={32}
-							alt='Avatar of the user'
-						/>
+					<div className={styles.info}>
+						<div className={styles.used}>
+							Использовано компаний <span>34</span>
+						</div>
+						<div className={styles.limit}>
+							Лимит по компаниям <span>100</span>
+						</div>
 					</div>
-					<button className={styles.btnLogout}>Выйти</button>
+
+					<div className={styles.profile}>
+						<div className={styles.profileInfo}>
+							<div className={styles.name}>Алексей А. </div>
+							<Image
+								src={avatar}
+								width={32}
+								height={32}
+								alt='Avatar of the user'
+							/>
+						</div>
+						<button
+							className={styles.btnLogout}
+							onClick={() => push('/logout')}
+						>
+							Выйти
+						</button>
+					</div>
 				</div>
 			)}
 		</>
