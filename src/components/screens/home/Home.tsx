9c5ddec4e9d400948b81377eefa7f857a98dcebe
Ivan <ivanvolodin960@gmail.com>
2023-08-19 18:51:24 +0300

    feat: add main section in component Home

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,8 +1,15 @@
+import cn from 'clsx'
 import localFont from 'next/font/local'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { FC } from 'react'
+
+import Button from '@/components/ui/button/Button'
 
 import Layout from '@/components/layout/Layout'
 import { IMeta } from '@/components/seo/meta.interface'
+
+import mainImage from '../../../../public/images/main.png'
 
 import styles from './Home.module.scss'
 
@@ -17,10 +24,34 @@ const Home: FC = () => {
 		description: 'Check business correctly'
 	}
 
+	const { push } = useRouter()
+
 	return (
 		<Layout meta={meta}>
-			<div className={styles.title}>Home</div>
-			<div className={ferryFont.className}>Page</div>
+			<div className={styles.home}>
+				<section className={styles.sectionMain}>
+					<div className={styles.info}>
+						<h1 className={cn(ferryFont.className, styles.title)}>
+							сервис по поиску публикаций о компании <br />
+							по его ИНН
+						</h1>
+						<h4 className={styles.description}>
+							Комплексный анализ публикаций, получение данных в формате PDF на
+							электронную почту.
+						</h4>
+						<Button size={'medium'} clickHandler={() => push('/getdata')}>
+							Запросить данные
+						</Button>
+					</div>
+					<Image
+						className={styles.mainImage}
+						src={mainImage}
+						width={560}
+						height={520}
+						alt='Main image'
+					/>
+				</section>
+			</div>
 		</Layout>
 	)
 }
