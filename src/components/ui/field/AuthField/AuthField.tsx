637a3fc4edfdc6a3ca25fe86b6e523ee8c9ac45f
Ivan <ivanvolodin960@gmail.com>
2023-08-20 19:10:26 +0300

    feat: create ui component AuthField

---
+++ src/components/ui/field/AuthField/AuthField.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react'
+import { Message, Path, UseFormRegister, ValidationRule } from 'react-hook-form'
+
+import styles from '../Field.module.scss'
+
+import { IAuthFields } from '@/interfaces/form.interface'
+
+interface IAuthFieldProps {
+	id: string
+	label: string
+	placeholder: string
+	type: string
+	register: UseFormRegister<IAuthFields>
+	name: Path<IAuthFields>
+	required: Message | ValidationRule<boolean>
+	pattern?: ValidationRule<RegExp>
+	error?: string
+}
+
+const AuthField: FC<IAuthFieldProps> = ({
+	id,
+	label,
+	register,
+	name,
+	required,
+	placeholder,
+	type,
+	error,
+	pattern
+}) => {
+	return (
+		<div style={{ marginBottom: '1rem' }}>
+			<label htmlFor={id} className={styles.label}>
+				{label}
+			</label>
+			<input
+				id={id}
+				placeholder={placeholder}
+				type={type}
+				{...register(name, { required, pattern })}
+				className={styles.input}
+			/>
+			{error && <div className='error'>{error}</div>}
+		</div>
+	)
+}
+
+export default AuthField
