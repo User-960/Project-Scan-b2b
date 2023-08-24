2550f33a9204920f5ffb1d3f0141ff5d08745a4a
Ivan <ivanvolodin960@gmail.com>
2023-08-25 02:52:40 +0300

    feat: create function helper for carousel

--- src/components/screens/search-result/result-carousel/ResultCarousel.tsx
+++ src/components/screens/search-result/result-carousel/ResultCarousel.tsx
@@ -1,35 +1,90 @@
 import { Carousel } from 'antd'
+import moment from 'moment'
+import 'moment/locale/ru'
 import { FC } from 'react'
 
 import { useObject } from '@/components/hooks/useObject'
 
 import styles from './ResultCarousel.module.scss'
+import { TypeHistograms } from '@/interfaces/objectsearch.interface'
 
-interface IResultCarouselProps {
-	data?: any
+interface IDayHistogram {
+	id: number
+	date: string
+	totalDocs: number
+	riskFact: number
 }
 
-const ResultCarousel: FC<IResultCarouselProps> = ({ data }) => {
+const ResultCarousel: FC = () => {
 	const { histogramsData } = useObject()
 
 	const onChange = (currentSlide: number) => {
 		console.log(currentSlide)
 	}
 
+	const filterHistogramsData = (histogramsData: TypeHistograms) => {
+		let allDaysHistogram: IDayHistogram[] = []
+		let dates: string[] = []
+		let totalDocs: number[] = []
+		let riskFact: number[] = []
+
+		if (histogramsData) {
+			for (let i = 0; i < histogramsData.data.length; i++) {
+				if (histogramsData.data[i].histogramType === 'totalDocuments') {
+					for (let j = 0; j < histogramsData.data[i].data.length; j++) {
+						dates.push(histogramsData.data[i].data[j].date)
+					}
+				}
+			}
+
+			for (let i = 0; i < histogramsData.data.length; i++) {
+				if (histogramsData.data[i].histogramType === 'totalDocuments') {
+					for (let j = 0; j < histogramsData.data[i].data.length; j++) {
+						totalDocs.push(histogramsData.data[i].data[j].value)
+					}
+				}
+
+				if (histogramsData.data[i].histogramType === 'riskFactors') {
+					for (let j = 0; j < histogramsData.data[i].data.length; j++) {
+						riskFact.push(histogramsData.data[i].data[j].value)
+					}
+				}
+			}
+		}
+
+		let allDaysHistogramDates = []
+		let allDaysHistogramTotal = []
+
+		for (let i = 0; i < dates.length; i++) {
+			allDaysHistogramDates.push({ date: dates[i] })
+		}
+
+		allDaysHistogramTotal = allDaysHistogramDates.map((item, index) => ({
+			...item,
+			totalDocs: totalDocs[index]
+		}))
+
+		allDaysHistogram = allDaysHistogramTotal.map((item, index) => ({
+			...item,
+			id: index,
+			riskFact: riskFact[index]
+		}))
+
+		return allDaysHistogram
+	}
+
+	console.log(filterHistogramsData(histogramsData))
+
 	const settings = {
 		arrows: true,
 		dots: false,
-		slidesToShow: 6,
+		slidesToShow: filterHistogramsData(histogramsData).length,
 		slidesToScroll: 1,
 		className: 'carouselHistograms'
 	}
 
 	return (
 		<>
-			<ul>
-				{histogramsData?.data.map((item: any) => <li>{item.histogramType}</li>)}
-			</ul>
-
 			<div className={styles.wrapperCarousel}>
 				<div className={styles.carouselTable}>
 					<div className={styles.carouselRow}>Период</div>
@@ -37,41 +92,17 @@ const ResultCarousel: FC<IResultCarouselProps> = ({ data }) => {
 					<div className={styles.carouselRow}>Риски</div>
 				</div>
 				<Carousel afterChange={onChange} {...settings}>
-					<div className={styles.carouselItem}>
-						<p className={styles.carouselDate}>10.09.2021</p>
-						<p className={styles.carouselAll}>5</p>
-						<p className={styles.carouselRisks}>0</p>
-					</div>
-					<div className={styles.carouselItem}>
-						<p className={styles.carouselDate}>10.09.2021</p>
-						<p className={styles.carouselAll}>5</p>
-						<p className={styles.carouselRisks}>0</p>
-					</div>
-					<div className={styles.carouselItem}>
-						<p className={styles.carouselDate}>10.09.2021</p>
-						<p className={styles.carouselAll}>5</p>
-						<p className={styles.carouselRisks}>0</p>
-					</div>
-					<div className={styles.carouselItem}>
-						<p className={styles.carouselDate}>10.09.2021</p>
-						<p className={styles.carouselAll}>5</p>
-						<p className={styles.carouselRisks}>0</p>
-					</div>
-					<div className={styles.carouselItem}>
-						<p className={styles.carouselDate}>10.09.2021</p>
-						<p className={styles.carouselAll}>5</p>
-						<p className={styles.carouselRisks}>0</p>
-					</div>
-					<div className={styles.carouselItem}>
-						<p className={styles.carouselDate}>10.09.2021</p>
-						<p className={styles.carouselAll}>5</p>
-						<p className={styles.carouselRisks}>0</p>
-					</div>
-					<div className={styles.carouselItem}>
-						<p className={styles.carouselDate}>10.09.2021</p>
-						<p className={styles.carouselAll}>5</p>
-						<p className={styles.carouselRisks}>0</p>
-					</div>
+					{filterHistogramsData(histogramsData).map((item: IDayHistogram) => (
+						<div key={item.id} className={styles.carouselItem}>
+							<p className={styles.carouselDate}>
+								{moment(item.date).format('L')}
+							</p>
+
+							<p className={styles.carouselAll}>{item.totalDocs}</p>
+
+							<p className={styles.carouselRisks}>{item.riskFact}</p>
+						</div>
+					))}
 				</Carousel>
 			</div>
 		</>
