0abe2a9d57c04229be38dcb396712636ba57d30f
Ivan <ivanvolodin960@gmail.com>
2023-08-27 20:36:31 +0300

    fix: fix styles for CardDocument

--- src/components/screens/search-result/card-document/CardDocument.tsx
+++ src/components/screens/search-result/card-document/CardDocument.tsx
@@ -20,20 +20,43 @@ interface ICardDocumentProps {
 const CardDocument: FC<ICardDocumentProps> = ({ doc }) => {
 	const { push } = useRouter()
 
-	const formatXMlText = (strXML: string): any => {
-		let test: string = ''
-		const subString = '<br><img src='
-		const subImage = `<br><img src="https://dummyimage.com/480x240">`
-		if (strXML) {
-			parseString(strXML, function (err: any, result: any) {
-				test = result.scandoc
-				console.log(test)
-				// test ? test : (test = `${subImage}`)
-			})
-		}
-	}
+	// const formatXMlText = (strXML: string): any => {
+	// 	let test: any
+	// 	let newText
+	// 	if (strXML) {
+	// 		parseString(strXML, function (err: any, result: any) {
+	// 			test = result.scandoc.sentence
+	// 			for (let i = 0; i < test.length; i++) {
+	// 				let partStr
+	// 				if (typeof test[i] !== 'string') {
+	// 					partStr = test[i].entity
+	// 				}
+	// 			}
+	// 			console.log(test)
+	// 			// test ? test : (test = `${subImage}`)
+	// 		})
+	// 	}
+	// }
 
-	formatXMlText(doc.content.markup)
+	// const formatXMlText = (strXML: string): any => {
+	// 	let test: any
+	// 	let newText: any
+	// 	if (strXML) {
+	// 		parseString(strXML, function (err: any, result: any) {
+	// 			test = result.scandoc.sentence
+	// 			console.log(test)
+	// 			// test ? test : (test = `${subImage}`)
+	// 		})
+	// 	}
+	// 	const textArticle = document.getElementById('textArticle')
+	// 	for (let i = 0; i < test.length; i++) {
+	// 		// textArticle?.innerHTML(test[i])
+	// 		for (let j = 0; j < test[i].length; j++) {
+	// 			newText.push(test[i][j].join())
+	// 		}
+	// 	}
+	// 	console.log(newText)
+	// }
 
 	const formatXMlImage = (strXML: string): any => {
 		let test: string = ''
@@ -66,22 +89,37 @@ const CardDocument: FC<ICardDocumentProps> = ({ doc }) => {
 				</Link>
 			</div>
 			<h6 className={styles.title}>{doc.title.text}</h6>
-			<div className={styles.cardBadge}>Технические новости</div>
+			<div className={styles.badges}>
+				{doc.attributes.isTechNews && (
+					<div className={styles.cardBadge}>Технические новости</div>
+				)}
+				{doc.attributes.isAnnouncement && (
+					<div className={styles.cardBadge}>Объявление</div>
+				)}
+				{<div className={styles.cardBadge}>Дайджест</div>}
+			</div>
 			<Image
 				className={styles.cardImage}
 				src={formatXMlImage(doc.content.markup)}
 				alt='Card Image'
 				width={550}
-				height={290}
+				height={270}
 				priority={true}
 			/>
 			<div className={styles.textInfo}>
-				{/* <p className={styles.text}>
-					{parserStr.parseFromString(
-						doc.content.markup,
-						'application/xml'
-					)}
+				{/* <p id='textArticle' className={styles.text}>
+					{formatXMlText(doc.content.markup)}
 				</p> */}
+				<p className={styles.text}>{doc.title.text}</p>
+				<br />
+				<p className={styles.text}>
+					Принципы SkillFactory: акцент на практике, забота о студентах и
+					ориентир на трудоустройство. 80% обучения — выполнение упражнений и
+					реальных проектов. Каждого студента поддерживают менторы, 2
+					саппорт-линии и комьюнити курса. А карьерный центр помогает составить
+					резюме, подготовиться к собеседованиям и познакомиться с
+					IT-рекрутерами.
+				</p>
 			</div>
 			<div className={styles.serviceBtn}>
 				<div className={styles.btnWrapper}>
