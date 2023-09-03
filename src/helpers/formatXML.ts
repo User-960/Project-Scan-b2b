2cf6bc6df5aade0948aea2bcdc8ba457f5416ce1
Ivan <ivanvolodin960@gmail.com>
2023-09-03 19:32:36 +0300

    fix: fix method for formating xml

--- src/helpers/formatXML.ts
+++ src/helpers/formatXML.ts
@@ -1,4 +1,25 @@
-const parseString = require('xml2js').parseString
+// const formatXMlImage = (strXML: string): any => {
+// 	let test: string = ''
+// 	const subString = '<br><img src='
+// 	const subImage = `<br><img src="./images/img-none.svg">`
+// 	if (strXML) {
+// 		parseString(strXML, function (err: any, result: any) {
+// 			test = result.scandoc._
+// 			test ? test : (test = `${subImage}`)
+// 		})
+// 	}
+// 	return test.length < 360
+// 		? test.split(subString).join().split('>').join().slice(2, -2)
+// 		: (test = `${subImage}`)
+// 				.split(subString)
+// 				.join()
+// 				.split('>')
+// 				.join()
+// 				.slice(2, -2)
+// }
+//-----------------------------------------------------------------
+
+const xml2js = require('xml2js')
 
 export const formatXMlImage = (strXML: string): any => {
 	let imgStr: string = ''
@@ -7,7 +28,7 @@ export const formatXMlImage = (strXML: string): any => {
 	const subImage = `<br><img src="./images/img-none.svg">`
 
 	if (strXML) {
-		parseString(strXML, function (err: any, result: any) {
+		xml2js.parseString(strXML, function (err: any, result: any) {
 			imgStr = result.scandoc._
 
 			if (imgStr) {
@@ -24,85 +45,30 @@ export const formatXMlImage = (strXML: string): any => {
 	return srcString
 }
 
-// const formatXMlImage = (strXML: string): any => {
-// 	let test: string = ''
-// 	const subString = '<br><img src='
-// 	const subImage = `<br><img src="./images/img-none.svg">`
-// 	if (strXML) {
-// 		parseString(strXML, function (err: any, result: any) {
-// 			test = result.scandoc._
-
-// 			test ? test : (test = `${subImage}`)
-// 		})
-// 	}
-
-// 	return test.length < 360
-// 		? test.split(subString).join().split('>').join().slice(2, -2)
-// 		: (test = `${subImage}`)
-// 				.split(subString)
-// 				.join()
-// 				.split('>')
-// 				.join()
-// 				.slice(2, -2)
-// }
-
 export const formatXMlText = (strXML: string): any => {
-	let formatText: string = ''
+	let formatXML: string = ''
+	const parser = new DOMParser()
+	let xmlDoc
 
 	if (strXML) {
-		parseString(
-			strXML,
-			{
-				trim: true,
-				normalize: true,
-				emptyTag: () => ({}),
-				ignoreAttrs: true
-			},
-			function (err: any, result: any) {
-				// console.log(result.scandoc)
+		xmlDoc = parser.parseFromString(strXML, 'text/xml')
+		formatXML = new XMLSerializer()
+			.serializeToString(xmlDoc)
+			.trim()
+			.replace(/(<([^>]+)>)/gi, '')
+			.replace(/[a-zA-Z]/g, '')
+			.replace(/[&]|[#]|[;]|[:]|[/]|[=]/g, '')
+			.replace(/""/g, '')
+			.replace(/"-"/g, '')
+			.replace(/_/g, '')
+			.replace(/[.]/g, '. ')
+			.replace(/[.]/g, '. ')
+			.replace(/".3"/g, '')
+			.replace(/(.?-)/g, '')
+			.replace(/(\d+%)/g, '')
+			.replace(/[0-9]{5}/g, '')
+			.slice(1, 500)
 
-				for (let i = 0; i < result.scandoc.sentence.length - 4; i++) {
-					if (typeof result.scandoc.sentence[i] === 'string') {
-						formatText += ` ${result.scandoc.sentence[i].trim()}`
-					}
-
-					if (typeof result.scandoc.sentence[i]._ === 'string') {
-						formatText += ` ${result.scandoc.sentence[i]._.trim()}`
-					}
-
-					if (result.scandoc.sentence[i].entity) {
-						if (typeof result.scandoc.sentence[i].entity[0] === 'string') {
-							formatText += ` ${result.scandoc.sentence[i].entity[0].trim()}`
-						}
-
-						if (
-							result.scandoc.sentence[i].entity[0].entity !== undefined &&
-							typeof result.scandoc.sentence[i].entity[0].entity[0].entity ===
-								'string'
-						) {
-							if (
-								result.scandoc.sentence[i].entity[0].entity[0].entity !==
-									undefined &&
-								typeof result.scandoc.sentence[i].entity[0].entity[0].entity ===
-									'string'
-							) {
-								formatText += ` ${result.scandoc.sentence[i].entity[0].entity[0].entity}`
-								if (
-									typeof result.scandoc.sentence[i].entity[0].entity[0]
-										.entity[0]._ === 'string' &&
-									result.scandoc.sentence[i].entity[0].entity[0].entity[0]._ !==
-										undefined
-								) {
-									formatText +=
-										result.scandoc.sentence[i].entity[0].entity[0].entity[0]._
-								}
-							}
-						}
-					}
-				}
-			}
-		)
-
-		return formatText.replace(/(<([^>]+)>)/gi, '').replace(/[()]/g, '')
+		return (formatXML += '...')
 	}
 }
